6f9e62ef38a5a930c31b529c3db4e190a6ca40cd
Bastien DURAN <bastienduran@mac.com>
2018-11-23 17:57:48 +0100

    feat(tests): use snapshot testing through enzyme

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+
+import { shallow } from 'enzyme';
 
 it('renders without crashing', () => {
   const props = {
@@ -15,6 +16,6 @@ it('renders without crashing', () => {
       },
     ],
   };
-  const div = document.createElement('div');
-  ReactDOM.render(<App {...props} />, div);
+  const Component = shallow(<App {...props} />);
+  expect(Component).toMatchSnapshot();
 });
