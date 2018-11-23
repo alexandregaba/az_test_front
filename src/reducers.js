d1ffffb48959dd4dbdaea11595e38c177df94072
Bastien DURAN <bastienduran@mac.com>
2018-11-23 21:32:07 +0100

    feat(edit-list): add case in reducers for start edit mode

--- src/reducers.js
+++ src/reducers.js
@@ -1,16 +1,23 @@
 import Immutable from 'immutable';
 import { combineReducers } from 'redux';
-import { ADD_GROCERY_ITEM } from './actions';
+import { ADD_GROCERY_ITEM, START_EDIT_GROCERY_ITEM } from './actions';
 
 const initialState = new Immutable.List([
-  { checked: false, name: 'Apples' },
-  { checked: false, name: 'Bananas' },
+  { checked: false, name: 'Apples', id: 1, editMode: false },
+  { checked: false, name: 'Bananas', id: 2, editMode: false },
 ]);
 
 export function groceries(state = initialState, action) {
   switch (action.type) {
-    case ADD_GROCERY_ITEM:
-      return state.push(action.item);
+    case ADD_GROCERY_ITEM: {
+      return new Immutable.List([...state, action.item]);
+    }
+    case START_EDIT_GROCERY_ITEM: {
+      const newState = state.map(
+        item => (item.id === action.item.id ? { ...item, editMode: true } : item),
+      );
+      return newState;
+    }
     default:
       return state;
   }
--- src/reducers.test.js
+++ src/reducers.test.js
@@ -6,8 +6,8 @@ describe('test reducers', () => {
     const action = { item: 'test', type: 'test' };
     const state = undefined;
     const result = new Immutable.List([
-      { checked: false, name: 'Apples' },
-      { checked: false, name: 'Bananas' },
+      { checked: false, name: 'Apples', id: 1, editMode: false },
+      { checked: false, name: 'Bananas', id: 2, editMode: false },
     ]);
     expect(groceries(state, action)).toEqual(result);
   });
@@ -30,12 +30,32 @@ describe('test reducers', () => {
   });
 
   it('returns Intial State plus the item from action', () => {
-    const action = { item: 'test', type: 'ADD_GROCERY_ITEM' };
+    const action = { item: { checked: false, name: 'Oranges' }, type: 'ADD_GROCERY_ITEM' };
     const state = undefined;
     const result = new Immutable.List([
-      { checked: false, name: 'Apples' },
-      { checked: false, name: 'Bananas' },
-      'test',
+      { checked: false, name: 'Apples', id: 1, editMode: false },
+      { checked: false, name: 'Bananas', id: 2, editMode: false },
+      { checked: false, name: 'Oranges' },
+    ]);
+    expect(groceries(state, action)).toEqual(result);
+  });
+
+  it('returns item in editMode', () => {
+    const action = { item: { id: 1 }, type: 'START_EDIT_GROCERY_ITEM' };
+    const state = undefined;
+    const result = new Immutable.List([
+      { checked: false, name: 'Apples', id: 1, editMode: true },
+      { checked: false, name: 'Bananas', id: 2, editMode: false },
+    ]);
+    expect(groceries(state, action)).toEqual(result);
+  });
+
+  it('returns unchanged state if id is not found', () => {
+    const action = { item: { id: 3 }, type: 'START_EDIT_GROCERY_ITEM' };
+    const state = undefined;
+    const result = new Immutable.List([
+      { checked: false, name: 'Apples', id: 1, editMode: false },
+      { checked: false, name: 'Bananas', id: 2, editMode: false },
     ]);
     expect(groceries(state, action)).toEqual(result);
   });
