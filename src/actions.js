c371a8654817d1cba6f6dffef2f806d55a164e24
Bastien DURAN <bastienduran@mac.com>
2018-11-23 21:28:07 +0100

    feat(edit-list): add actions to start or stop edit mode for an item

--- src/actions.js
+++ src/actions.js
@@ -1,5 +1,15 @@
 export const ADD_GROCERY_ITEM = 'ADD_GROCERY_ITEM';
+export const START_EDIT_GROCERY_ITEM = 'START_EDIT_GROCERY_ITEM';
+export const STOP_EDIT_GROCERY_ITEM = 'STOP_EDIT_GROCERY_ITEM';
 
 export function addGroceryItem(item) {
   return { type: ADD_GROCERY_ITEM, item };
+}
+
+export function startEditGroceryItem(item) {
+  return { type: START_EDIT_GROCERY_ITEM, item };
+}
+
+export function stopEditGroceryItem(item) {
+  return { type: STOP_EDIT_GROCERY_ITEM, item };
 }
--- src/actions.test.js
+++ src/actions.test.js
@@ -1,8 +1,25 @@
-import { addGroceryItem } from './actions';
+import {
+  addGroceryItem,
+  ADD_GROCERY_ITEM,
+  startEditGroceryItem,
+  START_EDIT_GROCERY_ITEM,
+  stopEditGroceryItem,
+  STOP_EDIT_GROCERY_ITEM,
+} from './actions';
 
 describe('test actions', () => {
-  it('addGrocery triggers action', () => {
+  it('triggers add item action', () => {
     const item = 'test';
-    expect(addGroceryItem(item)).toEqual({ item: 'test', type: 'ADD_GROCERY_ITEM' });
+    expect(addGroceryItem(item)).toEqual({ item: 'test', type: ADD_GROCERY_ITEM });
+  });
+
+  it('triggers START_EDIT_GROCERY_ITEM action', () => {
+    const item = 'test';
+    expect(startEditGroceryItem(item)).toEqual({ item: 'test', type: START_EDIT_GROCERY_ITEM });
+  });
+
+  it('triggers STOP_EDIT_GROCERY_ITEM action', () => {
+    const item = 'test';
+    expect(stopEditGroceryItem(item)).toEqual({ item: 'test', type: STOP_EDIT_GROCERY_ITEM });
   });
 });
